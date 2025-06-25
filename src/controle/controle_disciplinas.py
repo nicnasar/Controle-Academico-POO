e9e02feb48ef40b82a574dfb7c751c1ddabc885f
Nicolas Nasário Ferreira <nicnasar@gmail.com>
2025-06-24 21:22:25 -0300

    Commit do Desespero (falta muita coisa)

---
+++ src/controle/controle_disciplinas.py
@@ -0,0 +1,33 @@
+"""
+- Cadastrar Nova Disciplina: Formulário com campos de entrada para código, 
+nome, carga horária e nome do professor. 
+
+OBS  1:  O  cadastro  da  disciplina  deve  ser  feito  no  banco  de  dados  na 
+Tabela Disciplina. 
+
+OBS  2:  Seu  programa  não  deve  permitir  que  duas  disciplinas  com  o 
+mesmo código sejam cadastradas. 
+
+
+-  Atualizar  Disciplina:  Campo  para  digitar  o  código  da  disciplina.  Caso  exista, 
+exibir os dados atuais nos campos do formulário para edição. 
+
+- OBS 1: O atributo código não pode ser atualizado.  
+
+- OBS 2: Caso o usuário não deseje atualizar algum campo basta deixá-lo 
+em branco. Logo, deve-se manter o valor atual. 
+
+
+- Remover  Disciplina:  Campo  para  digitar  o  código  da  disciplina  e  botão  de 
+exclusão. 
+
+- OBS: Caso seja digitado um código de uma disciplina que não existe no 
+banco de dados, deve-se exibir uma mensagem avisando. 
+ 
+- Listar Disciplinas: Exibir uma tabela com as disciplinas cadastradas. Ao final da 
+página, deve ter um botão para possibilitar exportar os dados das disciplinas em 
+formato  Comma-Separated  Values  (CSV).  O  arquivo  deve  ser  estruturado  com 
+cada disciplina em uma linha e os atributos como colunas separadas por ponto-e-
+vírgula.
+
+"""
