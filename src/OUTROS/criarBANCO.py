4900b123b01995f2cc4848a0b2afb5fc46efb374
Nicolas Nasário <nicnasar@gmail.com>
2025-06-29 11:57:07 -0300

    Banco configurado

--- src/OUTROS/criarBANCO.py
+++ src/OUTROS/criarBANCO.py
@@ -1,5 +1,35 @@
 import sqlite3
 
+# com base em https://www.sqlitetutorial.net/sqlite-python/creating-tables/
+
+tabelas_sqlite = [
+    """CREATE TABLE IF NOT EXISTS 
+        Disciplina (
+            codigo INTEGER UNIQUE NOT NULL,
+            nome TEXT NOT NULL,
+            carga_horaria INTEGER NOT NULL,
+            nome_professor TEXT NOT NULL
+        );""",
+        
+    """CREATE TABLE IF NOT EXISTS
+        Aluno (
+            nome TEXT NOT NULL,
+            cpf TEXT NOT NULL UNIQUE,
+            idade INTEGER NOT NULL,
+            email TEXT NOT NULL,
+            endereco TEXT NOT NULL
+        );""",
+        
+    """CREATE TABLE IF NOT EXISTS
+        Matricula (
+            codigo_disciplina INTEGER NOT NULL,
+            cpf_aluno TEXT UNIQUE NOT NULL,
+            data_matricula TEXT NOT NULL,
+            horario_matricula TEXT NOT NULL
+        );"""
+]
+
+
 class CriarBanco:
     def __init__(self, caminho_banco):
         self.caminho_banco = caminho_banco
@@ -9,14 +39,9 @@ class CriarBanco:
     def _criar_banco(self):
         conexao = sqlite3.connect(self.caminho_banco)
         cursor = conexao.cursor()
-        cursor.execute("""
-            CREATE TABLE IF NOT EXISTS 
-                Aluno (
-                    nome INTEGER ,
-                    cpf INTEGER UNIQUE,
-                    idade TEXT,
-                    email TEXT,
-                    endereco TEXT
-            )
-        """)
+        
+        # cria cada uma das tabelas contidas na lista
+        for tabela in tabelas_sqlite:        
+            cursor.execute(tabela)
+            
         conexao.commit()
