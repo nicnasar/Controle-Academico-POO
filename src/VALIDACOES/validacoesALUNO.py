24818f4e7977796ff6c834c7fd22a13f968cfbd5
nicnasar <nicnasar@gmail.com>
2025-07-01 22:01:42 -0300

    Valores vazios Criado

--- src/VALIDACOES/validacoesALUNO.py
+++ src/VALIDACOES/validacoesALUNO.py
@@ -27,3 +27,35 @@ class ValidarAluno:
         return False
     
     
+    def valores_vazios(self, aluno: AlunoModelo):
+        
+        conexao = sqlite3.connect(self.caminho_banco)
+        cursor = conexao.cursor()
+        
+        if aluno.nome is None:
+            cursor.execute("SELECT nome FROM Aluno WHERE cpf = ?", (aluno.cpf,))
+            nome_atual = cursor.fetchone()
+            aluno.nome = nome_atual[0]
+            
+        if aluno.idade is None:
+            cursor.execute("SELECT idade FROM Aluno WHERE cpf = ?", (aluno.cpf,))
+            idade_atual = cursor.fetchone()
+            aluno.idade = idade_atual[0]
+            
+        if aluno.email is None:
+            cursor.execute("SELECT email FROM Aluno WHERE cpf = ?", (aluno.cpf,))
+            email_atual = cursor.fetchone()
+            aluno.email = email_atual[0]
+            
+            
+        if aluno.endereco is None:
+            cursor.execute("SELECT endereco FROM Aluno WHERE cpf = ?", (aluno.cpf,))
+            endereco_atual = cursor.fetchone()
+            aluno.endereco = endereco_atual[0]
+            
+        conexao.commit()
+        conexao.close()
+        
+        return aluno
+            
+        
